4bf9bb4c57fc34f977809da5678d82eeb8cbefcb
sukechannnn <wonderfulworld0315@gmail.com>
2021-11-15 19:39:24 +0900

    Setup react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Counter } from "./features/counter/counter";
+import { Todo } from "./features/todo/todo";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Counter />
+      <Routes>
+        <Route path="/" element={<Counter />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/features/todo/todo.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Todo: React.FC = () => {
+  return <h1>Todo</h1>;
+};
